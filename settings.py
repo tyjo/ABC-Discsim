d73360dcbb8517245ab6b9f4317ede5d1ee88afb
Tyler@Donut <tyjoseph03@gmail.com>
2015-07-14 12:07:51 -0400

    updated simulation code for discsim

--- settings.py
+++ settings.py
@@ -1,33 +1,36 @@
 # This defines samples within the landscape in ERCS. Each tuple defines
 # a coordinate where (0, 0) is the bottom left corner.
-sampleA = [(250, 750), (240, 740), (240, 760), (260, 740), (260, 760)]
-sampleB = [(750, 750), (740, 740), (740, 760), (760, 740), (760, 740)]
-sampleC = [(250, 250), (240, 240), (240, 260), (260, 240), (260, 260)]
-sampleD = [(750, 250), (740, 240), (740, 260), (760, 240), (760, 260)]
-sample = sampleA + sampleB + sampleC + sampleD
+sampleA = [(25, 75), (24, 74), (24, 76), (26, 74), (26, 76)]
+sampleB = [(75, 75), (74, 74), (74, 76), (76, 74), (76, 74)]
+#sampleC = [(250, 250), (240, 240), (240, 260), (260, 240), (260, 260)]
+#sampleD = [(750, 250), (740, 240), (740, 260), (760, 240), (760, 260)]
+sample = sampleA + sampleB# + sampleC + sampleD
 
 settings = {
 	### ERCS Settings ###
-	'landscape_size': 1000,
+	'length': 100, # torus side length
 	# Need to update GeneSamples_TEMPLATE.arp if you change this.
 	'sample_locations': sample,
+	'recombination_probability': 0.5,
+	'num_replicates': 1,
 
-	# The number of recombination proobabilities determines the number of loci.
-	'recombination_probabilities': [0.5, 0.5, 0.5, 0.5],
-	'num_replicates': 100000,
-	
-	# rate in events per 1000/gen
-	'small_event': {'rate': (1000.0, 1000.0), 'radius': (20, 20), 'u': 1.0},
-	'large_event': {'rate': (1.0, 100.0), 'radius': (50, 300), 'u': 0.25},
+	# If true, parameters for events are 
+	# set based on population density.
+	'estimate_population_size': True,
+	'population_size': (10000.0, 20000.0), # range of priors for population size
+	'small_event': { 'rate': 1.0, 'radius': 2 },
+
+	# Only used if estimate_population_size is False
+	# Estimate extinction rate / radius
+	'large_event': { 'rate': (10.0, 10000.0), 'radius': (5, 25) }, # range of priors for event rate
 	
 	### SeqGen Settings ###
 	'mutation_rate': 1.1e-8, # Base mutation rate
-	'evolution_rates': [1.02, 1.20, 1.23, 0.97, 0.88], 	# Scales mutation rate per loci.
-	'seq_length': '5000',
-	'num_partitions': '5',
-	'partitions': ['904', '779', '1447', '364', '1506'],
+	'seq_length': '10000',
+	'num_partitions': '10',
+	'partitions': ['1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'], #'1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'],
 
 	### Multiprocessing settings ###
 	# If set to 0, all CPUs are used
-	'num_cpus': 2,
+	'num_cpus': 1,
 }
