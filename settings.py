868e169ab9e998986db82c670c5a2a48c6dacd05
Tyler@Donut <tyjoseph03@gmail.com>
2015-07-15 15:50:17 -0400

    updated settings file

--- settings.py
+++ settings.py
@@ -1,36 +1,33 @@
 # This defines samples within the landscape in ERCS. Each tuple defines
 # a coordinate where (0, 0) is the bottom left corner.
-sampleA = [(25, 75), (24, 74), (24, 76), (26, 74), (26, 76)]
-sampleB = [(75, 75), (74, 74), (74, 76), (76, 74), (76, 74)]
-#sampleC = [(250, 250), (240, 240), (240, 260), (260, 240), (260, 260)]
-#sampleD = [(750, 250), (740, 240), (740, 260), (760, 240), (760, 260)]
-sample = sampleA + sampleB# + sampleC + sampleD
+sample = [ (12.5, 12.5), (37.5, 37.5), (62.5, 12.5), (87.5, 37.5),
+           (12.5, 62.5), (37.5, 87.5), (62.5, 62.5), (87.5, 87.5) ]
 
 settings = {
-	### ERCS Settings ###
-	'length': 100, # torus side length
-	# Need to update GeneSamples_TEMPLATE.arp if you change this.
-	'sample_locations': sample,
-	'recombination_probability': 0.5,
-	'num_replicates': 1,
+    ### ERCS Settings ###
+    'length': 100, # torus side length
+    # Need to update GeneSamples_TEMPLATE.arp if you change this.
+    'sample_locations': sample,
+    'recombination_probability': 0.5,
+    'num_replicates': 10000,
 
-	# If true, parameters for events are 
-	# set based on population density.
-	'estimate_population_size': True,
-	'population_size': (10000.0, 20000.0), # range of priors for population size
-	'small_event': { 'rate': 1.0, 'radius': 2 },
+    # If true, parameters for events are 
+    # set based on population density.
+    'estimate_population_size': True,
+    'population_size': (10000.0, 100000.0), # range of priors for population size
+    'small_event': { 'rate': 1.0, 'radius': 2 },
 
-	# Only used if estimate_population_size is False
-	# Estimate extinction rate / radius
-	'large_event': { 'rate': (10.0, 10000.0), 'radius': (5, 25) }, # range of priors for event rate
-	
-	### SeqGen Settings ###
-	'mutation_rate': 1.1e-8, # Base mutation rate
-	'seq_length': '10000',
-	'num_partitions': '10',
-	'partitions': ['1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'], #'1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'],
+    # Only used if estimate_population_size is False
+    # Estimate extinction rate / radius
+    'large_event': { 'rate': (10.0, 1000.0), 'radius': (5, 25) }, # range of priors for event rate
+    
+    ### SeqGen Settings ###
+    'mutation_rate': 1.1e-8, # Base mutation rate
+    'seq_length': '10000',
+    'num_partitions': '10',
+    'partitions': ['1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'], #'1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000', '1000'],
 
-	### Multiprocessing settings ###
-	# If set to 0, all CPUs are used
-	'num_cpus': 1,
+    ### Multiprocessing settings ###
+    # If set to 0, all CPUs are used
+    'num_cpus': 6,
 }
--- src/validate_settings.py
+++ src/validate_settings.py
@@ -1,65 +1,54 @@
 from sys import exit
 from settings import settings
 
-def validate():	
-	# Must specify valid partition lengths for each partition
-	try:
-		assert(int(settings['num_partitions']) == len(settings['partitions']))
-	except:
-		exit("Error in settings: num_partitions does not match partitions")
+def validate(): 
+    # Must specify valid partition lengths for each partition
+    try:
+        assert(int(settings['num_partitions']) == len(settings['partitions']))
+    except:
+        exit("Error in settings: num_partitions does not match partitions")
 
-	#try:
-#		assert(len(settings['evolution_rates']) == len(settings['partitions']))
-	#except:
-	#	exit("Error in settings: must specify evolution rate for every partition")
+    # Check sample locations in landscape
+    try:
+        torus_length = settings['length']
+        for sample in settings['sample_locations']:
+            assert(0 <= sample[0] and sample[0] < torus_length)
+            assert(0 <= sample[1] and sample[1] < torus_length)
+    except:
+        exit("Error in settings: sample_location defined out of bounds, 0 <= sample < length")
 
-	# Check sample locations in landscape
-	try:
-		torus_length = settings['length']
-		for sample in settings['sample_locations']:
-			assert(0 <= sample[0] and sample[0] < torus_length)
-			assert(0 <= sample[1] and sample[1] < torus_length)
-	except:
-		exit("Error in settings: sample_location defined out of bounds, 0 <= sample < length")
+    # Type checks
+    try:
+        assert type(settings['length']) is int, "length must be int"
+        assert type(settings['sample_locations']) is list, "sample_locations must be list of (int, int)" 
+        assert type(settings['recombination_probability']) is float, "recombination_probability must be double"
+        assert type(settings['num_replicates']) is int, "num_replicates must be int"
+        assert type(settings['small_event']) is dict, "small_event must be dict"
+        assert type(settings['large_event']) is dict, "large_event must be dict" 
+        assert type(settings['mutation_rate']) is float, "mutation_rate must be double"
+        assert type(settings['seq_length']) is str, "seq_length must be string"
+        assert type(settings['num_partitions']) is str, "num_partitions must be str"
+        assert type(settings['partitions']) is list, "partitions must be list of str"
+        assert type(settings['num_cpus']) is int, "num_cpus must be int"
 
-	# Type checks
-	try:
-		assert type(settings['length']) is int, "length must be int"
-		assert type(settings['sample_locations']) is list, "sample_locations must be list of (int, int)" 
-		assert type(settings['recombination_probability']) is float, "recombination_probability must be double"
-		assert type(settings['num_replicates']) is int, "num_replicates must be int"
-		assert type(settings['small_event']) is dict, "small_event must be dict"
-		assert type(settings['large_event']) is dict, "large_event must be dict" 
-		assert type(settings['mutation_rate']) is float, "mutation_rate must be double"
-		#assert type(settings['evolution_rates']) is list, "evolution_rate must be list"
-		assert type(settings['seq_length']) is str, "seq_length must be string"
-		assert type(settings['num_partitions']) is str, "num_partitions must be str"
-		assert type(settings['partitions']) is list, "partitions must be list of str"
-		assert type(settings['num_cpus']) is int, "num_cpus must be int"
+        # Check list internal types
+        for sample in settings['sample_locations']:
+            assert type(sample) is tuple, "sample_locations must be list of tuples of ints: [(int, int)]"
+            assert type(sample[0]) is int, "sample_locations must be integer values"
+            assert type(sample[1]) is int, "sample_locations must be integar values"
+            assert len(sample) is 2, "sample_locations must only contain two values (int, int)"
 
-		# Check list internal types
-		for sample in settings['sample_locations']:
-			assert type(sample) is tuple, "sample_locations must be list of tuples of ints: [(int, int)]"
-			assert type(sample[0]) is int, "sample_locations must be integer values"
-			assert type(sample[1]) is int, "sample_locations must be integar values"
-			assert len(sample) is 2, "sample_locations must only contain two values (int, int)"
+        assert type(settings['small_event']['rate']) is float, "small_event rate must be float"
+        assert type(settings['small_event']['radius']) is int, "small_event radius must be int"
 
-		#for rate in settings['evolution_rates']:
-		#	assert type(rate) is float, "evolution_rate must be list of float"
+        assert type(settings['large_event']['rate'][0]) is float, "large_event rate must be float"
+        assert type(settings['large_event']['rate'][1]) is float ,"large_event rate must be float"
+        assert len(settings['large_event']['rate']) == 2, "large_event rate can only take 2 priors"
+        assert settings['large_event']['rate'][0] <= settings['large_event']['rate'][1], "large_event prior lower bound must be less than upper bound"
 
-		assert type(settings['small_event']['rate']) is float, "small_event rate must be float"
-		assert type(settings['small_event']['radius']) is int, "small_event radius must be int"
-		#assert type(settings['small_event']['u']) is float, "small_event u must be float"
+        assert type(settings['large_event']['radius'][0]) is int, "large_event radius must be int"
+        assert type(settings['large_event']['radius'][1]) is int,"large_event radius must be int"
+        assert len(settings['large_event']['radius']) == 2, "large_event radius can only take 2 priors"
 
-		assert type(settings['large_event']['rate'][0]) is float, "large_event rate must be float"
-		assert type(settings['large_event']['rate'][1]) is float ,"large_event rate must be float"
-		assert len(settings['large_event']['rate']) == 2, "large_event rate can only take 2 priors"
-		assert settings['large_event']['rate'][0] <= settings['large_event']['rate'][1], "large_event prior lower bound must be less than upper bound"
-
-		assert type(settings['large_event']['radius'][0]) is int, "large_event radius must be int"
-		assert type(settings['large_event']['radius'][1]) is int,"large_event radius must be int"
-		assert len(settings['large_event']['radius']) == 2, "large_event radius can only take 2 priors"
-		#assert type(settings['large_event']['u']) is float, "large_event u must be float"
-
-	except AssertionError, e:
-		exit("Error in settings: {}".format(e.args[0]))
+    except AssertionError, e:
+        exit("Error in settings: {}".format(e.args[0]))
